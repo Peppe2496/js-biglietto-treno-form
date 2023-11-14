5b7449b3db549ad04462f16377de46c801f8883d
Peppe2496 <giuseppemusellajob@gmail.com>
2023-11-14 17:27:51 +0100

    finished

--- js/main.js
+++ js/main.js
@@ -1,2 +1,42 @@
 // Questo è il file che contiene tutto il codice JS della pagina web
+function FormCompleted(event){
+    event.preventDefault();
 
+    let nameSurname = document.getElementById("nameSurname").value;
+    let kilometersToDo = document.getElementById("kilometersToDo").value;
+    let age = document.getElementById("age").value;
+    let carriegeNumber = Math.floor(Math.random() * 11) + 1;
+    let cpNumber = Math.floor(Math.random() * 10001) + 1;
+
+    if (isNaN(kilometersToDo)){
+        alert("Deh peffozza");
+    }
+
+    kilometersToDo = parseInt(kilometersToDo);
+
+
+    let baseTicketPrice = kilometersToDo * 0.21;
+
+    if(age === "2") {
+        baseTicketPrice = baseTicketPrice-((baseTicketPrice*20)/100);
+        
+        
+    } else if (age === "3"){
+        baseTicketPrice = baseTicketPrice-((baseTicketPrice*40)/100);
+        
+        
+    } 
+
+    let finalPrice = baseTicketPrice.toFixed(2);
+    
+        
+    
+
+    document.getElementById("ticket-name").innerHTML = nameSurname;
+    document.getElementById("carriege-number").innerHTML = carriegeNumber;
+    document.getElementById("Cp-number").innerHTML = cpNumber;
+    document.getElementById("finalPrice").innerHTML = finalPrice + "€";
+
+    
+
+}
